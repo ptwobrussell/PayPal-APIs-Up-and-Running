495e757bc20484262e5dcc2a95724d7918c02770
Matthew A. Russell <matthew@zaffra.com>
2012-02-01 01:48:17 -0600

    Removing unnecessary fields (that would have been confusing since they are no longer used) from template config file in Appendix A.

--- appa/Tweet-Relevance/twitter_config.template.py
+++ appa/Tweet-Relevance/twitter_config.template.py
@@ -3,11 +3,3 @@
 
 CONSUMER_KEY = '' 
 CONSUMER_SECRET = ''
-
-# PayPal merchant settings
-# Get these "3 token api credentials" from the "API Credentials" Tab of the PayPal Sandbox that
-# you'll access from your PayPal *developer* account
-
-PP_API_USERNAME = ''
-PP_API_PASSWORD = ''
-PP_API_SIGNATURE = ''
